ce9cb997c1a179907edadabbc4eebd1145ef72ae
Lukas Povoda <lupo112@gmail.com>
2016-08-16 20:08:15 +0200

    Support wildcards

--- src/Compiler/BaseCompiler.php
+++ src/Compiler/BaseCompiler.php
@@ -3,6 +3,7 @@
 namespace Sw2\Load\Compiler;
 
 use Nette\Caching\Cache;
+use Nette\Utils\Strings;
 use Tracy\Debugger;
 
 /**
@@ -25,6 +26,9 @@ abstract class BaseCompiler implements ICompiler
 
 	/** @var array */
 	private $files;
+
+	/** @var array */
+	private $unpackedFiles = [];
 
 	/** @var array */
 	private $statistics = [];
@@ -64,28 +68,51 @@ abstract class BaseCompiler implements ICompiler
 		$files = $this->files[$name];
 		$files = is_string($files) ? [$files] : $files;
 		if ($path === NULL) {
-			$time = $this->getModifyTime($files);
-			$path = $this->genDir . '/' . $this->getOutputFilename($name, $files, $time, $this->debugMode);
+			$unpackedFiles = $this->unpack($name, $files);
+			$time = $this->getModifyTime($unpackedFiles);
+			$path = $this->genDir . '/' . $this->getOutputFilename($name, $unpackedFiles, $time, $this->debugMode);
 			$this->cache->save([$name, $this->debugMode], $path);
 		}
 
 		$genFile = "{$this->wwwDir}/$path";
-		if (!file_exists($genFile) || ($this->debugMode && filemtime($genFile) < (isset($time) ? $time : ($time = $this->getModifyTime($files))))) {
+		if (!file_exists($genFile) || ($this->debugMode && filemtime($genFile) < (isset($time) ? $time : ($time = $this->getModifyTime($this->unpack($name, $files)))))) {
 			$start = microtime(TRUE);
-			$parsedFiles = $this->compile($files, $genFile);
+			$parsedFiles = $this->compile($this->unpack($name, $files), $genFile);
 			if ($this->debugMode) {
 				$this->statistics[$name]['time'] = microtime(TRUE) - $start;
 				$this->statistics[$name]['parsedFiles'] = $parsedFiles;
 			}
 		}
 		if ($this->debugMode) {
+			$unpackedFiles = $this->unpack($name, $files);
 			$this->statistics[$name]['size'] = filesize($genFile);
-			$this->statistics[$name]['file'] = count($files) > 1 ? $files : reset($files);
-			$this->statistics[$name]['date'] = isset($time) ? $time : ($time = $this->getModifyTime($files));
+			$this->statistics[$name]['file'] = count($unpackedFiles) > 1 ? $unpackedFiles : reset($unpackedFiles);
+			$this->statistics[$name]['date'] = isset($time) ? $time : ($time = $this->getModifyTime($unpackedFiles));
 			$this->statistics[$name]['path'] = $path;
 		}
 
 		return $path;
+	}
+
+	/**
+	 * @param string $name
+	 * @param array $files
+	 * @return array
+	 */
+	private function unpack($name, array $files)
+	{
+		if (!isset($this->unpackedFiles[$name])) {
+			$unpackedFiles = [];
+			foreach ($files as $file) {
+				if (strpos($file, '*') !== FALSE) {
+					$unpackedFiles = array_merge($unpackedFiles, glob($file));
+				} else {
+					$unpackedFiles[] = $file;
+				}
+			}
+			$this->unpackedFiles[$name] = $unpackedFiles;
+		}
+		return $this->unpackedFiles[$name];
 	}
 
 	/**
